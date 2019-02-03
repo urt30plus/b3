fa79b8d159b45d61b53b21c5b1b3a2920d2d07eb
John Wagenleitner <john.wagenleitner@gmail.com>
2019-02-02 16:25:42 -0800

    minor formatting and remove warning about module import at top of file

--- b3_run.py
+++ b3_run.py
@@ -30,12 +30,11 @@ import sys
 if sys.version_info < (3, 6):
     raise SystemExit("B3 is not compatible with Python versions <3.6")
 
-import b3.run
-
 
 def main():
+    import b3.run
     b3.run.main()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
