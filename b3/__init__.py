08042a1023b357796f7913345b15aefe2c5aff4e
John Wagenleitner <john.wagenleitner@gmail.com>
2019-12-30 21:35:50 -0800

    bump version

--- b3/__init__.py
+++ b3/__init__.py
@@ -9,7 +9,7 @@ from tempfile import TemporaryFile
 import b3.functions
 
 __author__ = 'ThorN'
-__version__ = '3.31.12'
+__version__ = '3.31.13'
 
 modulePath = b3.functions.resource_directory(__name__)
 
