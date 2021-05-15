bf2c3a4054ec944764627780873dbcca31838fe8
Urt ThirtyPlus <urt30plus@gmail.com>
2021-05-15 15:03:32 -0500

    bump version

--- b3/__init__.py
+++ b3/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'ThorN'
-__version__ = '3.31.26'
+__version__ = '3.31.27'
 
-versionEdition = "TX"
+versionEdition = "Tex"
 versionId = f'v{__version__}'
 version = f'^0(^8b3^0) ^9{versionId} ^9[{versionEdition}^9]^3'
 
