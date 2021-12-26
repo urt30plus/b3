e5a8a693e603c7927200fa394a872c708d5e2910
Urt ThirtyPlus <urt30plus@gmail.com>
2021-12-26 09:47:41 -0800

    remove use of deprecated distutils version class

--- b3/update.py
+++ b3/update.py
@@ -1,7 +1,7 @@
-import distutils.version
 import os
-import re
 import time
+
+import packaging.version
 
 import b3
 import b3.config
@@ -9,7 +9,7 @@ import b3.functions
 import b3.storage
 
 
-class B3version(distutils.version.StrictVersion):
+class B3version(packaging.version.Version):
     """
     Version numbering for BigBrotherBot.
     Compared to version.StrictVersion this class allows version numbers such as :
@@ -22,107 +22,8 @@ class B3version(distutils.version.StrictVersion):
         1.0b
         1.0b1
         1.0b3
-        1.9.0dev7.daily21-20121004
     And make sure that any 'dev' prerelease is inferior to any 'alpha' prerelease
     """
-    version = None
-    prerelease = None
-    build_num = None
-
-    version_re = re.compile(r'''^
-(?P<major>\d+)\.(?P<minor>\d+)   # 1.2
-(?:\. (?P<patch>\d+))?           # 1.2.45
-(?P<prerelease>                  # 1.2.45b2
-  (?P<tag>a|b|dev)
-  (?P<tag_num>\d+)?
-)?                                                                     # 1.2.45b2.devd94d71a-20120901
-((?P<daily>\.daily(?P<build_num>\d+?))|(?P<dev>\.dev(?P<dev_num>\w+?)) # 1.2.45b2.daily4-20120901
-)?
-(?:-(?P<date>20\d\d\d\d\d\d))?   # 1.10.0dev-20150215
-$''', re.VERBOSE)
-    prerelease_order = {'dev': 0, 'a': 1, 'b': 2}
-
-    def parse(self, vstring):
-        """
-        Parse the version number from a string.
-        :param vstring: The version string
-        """
-        if not (match := self.version_re.match(vstring)):
-            raise ValueError(f"invalid version number '{vstring}'")
-
-        major = match.group('major')
-        minor = match.group('minor')
-
-        if patch := match.group('patch'):
-            self.version = tuple(map(int, [major, minor, patch]))
-        else:
-            self.version = tuple(list(map(int, [major, minor])) + [0])
-
-        if prerelease := match.group('tag'):
-            prerelease_num = match.group('tag_num')
-            self.prerelease = (prerelease, int(prerelease_num if prerelease_num else '0'))
-        else:
-            self.prerelease = None
-
-        if daily_num := match.group('build_num'):
-            self.build_num = int(daily_num if daily_num else '0')
-        else:
-            self.build_num = None
-
-    def __cmp__(self, other):
-        """
-        Compare current object with another one.
-        :param other: The other object
-        """
-        if isinstance(other, str):
-            other = B3version(other)
-
-        compare = b3.functions.cmp(self.version, other.version)
-        if compare != 0:
-            return compare
-
-        # we have to compare prerelease
-        compare = self.__cmp_prerelease(other)
-        if compare != 0:
-            return compare
-
-        # we have to compare build num
-        return self.__cmp_build(other)
-
-    def __gt__(self, other):
-        return self.__cmp__(other) > 0
-
-    def __lt__(self, other):
-        return self.__cmp__(other) < 0
-
-    def __cmp_prerelease(self, other):
-        # case 1: neither has prerelease; they're equal
-        # case 2: self has prerelease, other doesn't; other is greater
-        # case 3: self doesn't have prerelease, other does: self is greater
-        # case 4: both have prerelease: must compare them!
-        if not self.prerelease and not other.prerelease:
-            return 0
-        elif self.prerelease and not other.prerelease:
-            return -1
-        elif not self.prerelease and other.prerelease:
-            return 1
-        elif self.prerelease and other.prerelease:
-            return b3.functions.cmp((self.prerelease_order[self.prerelease[0]], self.prerelease[1]),
-                                    (self.prerelease_order[other.prerelease[0]], other.prerelease[1]))
-
-    def __cmp_build(self, other):
-        # case 1: neither has build_num; they're equal
-        # case 2: self has build_num, other doesn't; other is greater
-        # case 3: self doesn't have build_num, other does: self is greater
-        # case 4: both have build_num: must compare them!
-        if not self.build_num and not other.build_num:
-            return 0
-        elif self.build_num and not other.build_num:
-            return -1
-        elif not self.build_num and other.build_num:
-            return 1
-        elif self.build_num and other.build_num:
-            return b3.functions.cmp(self.build_num, other.build_num)
 
 
 class DBUpdate:
@@ -144,7 +45,8 @@ class DBUpdate:
                 )
         except b3.config.ConfigFileNotValid as cerr:
             print(cerr)
-            b3.functions.console_exit(f'ERROR: configuration file not valid ({config})')
+            b3.functions.console_exit(
+                f'ERROR: configuration file not valid ({config})')
 
     def run(self):
         """
@@ -170,14 +72,17 @@ class DBUpdate:
             :param update_version: the update version
             """
             if B3version(b3.__version__) >= update_version:
-                sql = b3.functions.getAbsolutePath(f'@b3/sql/{storage.protocol}/b3-update-{update_version}.sql')
+                sql = b3.functions.getAbsolutePath(
+                    f'@b3/sql/{storage.protocol}/b3-update-{update_version}.sql')
                 if os.path.isfile(sql):
                     try:
-                        print(f'>>> updating database to version {update_version}')
+                        print(
+                            f'>>> updating database to version {update_version}')
                         time.sleep(.5)
                         storage.queryFromFile(sql)
                     except Exception as err:
-                        print(f'WARNING: could not update database properly: {err}')
+                        print(
+                            f'WARNING: could not update database properly: {err}')
                         time.sleep(3)
 
         dsn = self.config.get('b3', 'database')
--- tests/plugins/poweradminurt/iourt43/__init__.py
+++ tests/plugins/poweradminurt/iourt43/__init__.py
@@ -3,12 +3,10 @@ import unittest
 from mockito import when
 
 from b3 import TEAM_UNKNOWN
-from b3 import __version__ as b3_version
 from b3.clients import Cvar
 from b3.config import XmlConfigParser
 from b3.parsers.iourt43 import Iourt43Parser
 from b3.plugins.admin import AdminPlugin
-from b3.update import B3version
 from tests import logging_disabled
 
 
@@ -32,12 +30,10 @@ class Iourt43TestCase(unittest.TestCase):
             self.console = Iourt43Parser(self.parser_conf)
             self.console.startup()
 
-            if B3version(b3_version) >= B3version("1.10dev"):
-                admin_plugin_conf_file = '@b3/conf/plugin_admin.ini'
-            else:
-                admin_plugin_conf_file = '@b3/conf/plugin_admin.xml'
+            admin_plugin_conf_file = '@b3/conf/plugin_admin.ini'
             with logging_disabled():
-                self.adminPlugin = AdminPlugin(self.console, admin_plugin_conf_file)
+                self.adminPlugin = AdminPlugin(self.console,
+                                               admin_plugin_conf_file)
                 self.adminPlugin.onLoadConfig()
                 self.adminPlugin.onStartup()
 
@@ -48,26 +44,42 @@ class Iourt43TestCase(unittest.TestCase):
 
             # prepare a few players
             from tests.fake import FakeClient
-            self.joe = FakeClient(self.console, name="Joe", exactName="Joe", guid="zaerezarezar", groupBits=1,
+            self.joe = FakeClient(self.console, name="Joe", exactName="Joe",
+                                  guid="zaerezarezar", groupBits=1,
                                   team=TEAM_UNKNOWN, teamId=0, squad=0)
-            self.simon = FakeClient(self.console, name="Simon", exactName="Simon", guid="qsdfdsqfdsqf", groupBits=0,
+            self.simon = FakeClient(self.console, name="Simon",
+                                    exactName="Simon", guid="qsdfdsqfdsqf",
+                                    groupBits=0,
                                     team=TEAM_UNKNOWN, teamId=0, squad=0)
-            self.reg = FakeClient(self.console, name="Reg", exactName="Reg", guid="qsdfdsqfdsqf33", groupBits=4,
+            self.reg = FakeClient(self.console, name="Reg", exactName="Reg",
+                                  guid="qsdfdsqfdsqf33", groupBits=4,
                                   team=TEAM_UNKNOWN, teamId=0, squad=0)
-            self.moderator = FakeClient(self.console, name="Moderator", exactName="Moderator", guid="sdf455ezr",
-                                        groupBits=8, team=TEAM_UNKNOWN, teamId=0, squad=0)
-            self.admin = FakeClient(self.console, name="Level-40-Admin", exactName="Level-40-Admin", guid="875sasda",
-                                    groupBits=16, team=TEAM_UNKNOWN, teamId=0, squad=0)
-            self.superadmin = FakeClient(self.console, name="God", exactName="God", guid="f4qfer654r", groupBits=128,
+            self.moderator = FakeClient(self.console, name="Moderator",
+                                        exactName="Moderator", guid="sdf455ezr",
+                                        groupBits=8, team=TEAM_UNKNOWN,
+                                        teamId=0, squad=0)
+            self.admin = FakeClient(self.console, name="Level-40-Admin",
+                                    exactName="Level-40-Admin", guid="875sasda",
+                                    groupBits=16, team=TEAM_UNKNOWN, teamId=0,
+                                    squad=0)
+            self.superadmin = FakeClient(self.console, name="God",
+                                         exactName="God", guid="f4qfer654r",
+                                         groupBits=128,
                                          team=TEAM_UNKNOWN, teamId=0, squad=0)
 
     def tearDown(self):
         self.console.shutdown()
 
     def init_default_cvar(self):
-        when(self.console).getCvar('timelimit').thenReturn(Cvar('timelimit', value=20))
-        when(self.console).getCvar('g_maxGameClients').thenReturn(Cvar('g_maxGameClients', value=16))
-        when(self.console).getCvar('sv_maxclients').thenReturn(Cvar('sv_maxclients', value=16))
-        when(self.console).getCvar('sv_privateClients').thenReturn(Cvar('sv_privateClients', value=0))
-        when(self.console).getCvar('g_allowvote').thenReturn(Cvar('g_allowvote', value=0))
-        when(self.console).getCvar('g_gear').thenReturn(Cvar('g_gear', value=''))
+        when(self.console).getCvar('timelimit').thenReturn(
+            Cvar('timelimit', value=20))
+        when(self.console).getCvar('g_maxGameClients').thenReturn(
+            Cvar('g_maxGameClients', value=16))
+        when(self.console).getCvar('sv_maxclients').thenReturn(
+            Cvar('sv_maxclients', value=16))
+        when(self.console).getCvar('sv_privateClients').thenReturn(
+            Cvar('sv_privateClients', value=0))
+        when(self.console).getCvar('g_allowvote').thenReturn(
+            Cvar('g_allowvote', value=0))
+        when(self.console).getCvar('g_gear').thenReturn(
+            Cvar('g_gear', value=''))
