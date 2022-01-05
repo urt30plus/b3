0b690d4e85e77521f545bb410e867ebed528129d
Urt ThirtyPlus <urt30plus@gmail.com>
2022-01-04 19:02:05 -0800

    replace function `stdout_write` with print and some minor refactoring

--- b3/__main__.py
+++ b3/__main__.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 import signal
-import sys
 
 import b3
 import b3.config
@@ -12,32 +11,27 @@ __author__ = 'ThorN'
 __version__ = '1.8'
 
 
-def stdout_write(message, flush=True):
-    sys.stdout.write(message)
-    if flush:
-        sys.stdout.flush()
-
-
 def start(mainconfig, options):
     """
     Main B3 startup.
-    :param mainconfig: The B3 configuration file instance :type: b3.config.MainConfig
+    :param mainconfig: The B3 configuration file instance b3.config.MainConfig
     :param options: command line options
     """
     b3.confdir = os.path.dirname(mainconfig.fileName)
 
-    stdout_write(f'Starting B3      : {b3.getB3versionString()}\n')
+    print(f'Starting B3      : {b3.getB3versionString()}', flush=True)
 
     # not real loading but the user will get what's configuration he is using
-    stdout_write(f'Loading config   : {b3.functions.getShortPath(mainconfig.fileName, True)}\n')
+    config_path = b3.functions.getShortPath(mainconfig.fileName, True)
+    print(f'Loading config   : {config_path}', flush=True)
 
-    parsertype = mainconfig.get('b3', 'parser')
-    stdout_write(f'Loading parser   : {parsertype}\n')
+    parser_type = mainconfig.get('b3', 'parser')
+    print(f'Loading parser   : {parser_type}', flush=True)
 
-    parser = b3.functions.loadParser(parsertype)
+    parser = b3.functions.loadParser(parser_type)
     b3.console = parser(mainconfig, options)
 
-    def termSignalHandler(signum, frame):
+    def term_signal_handler(signum, frame):
         """
         Define the signal handler so to handle B3 shutdown properly.
         """
@@ -45,9 +39,9 @@ def start(mainconfig, options):
         b3.console.shutdown()
 
     try:
-        # necessary if using the functions profiler,
-        # because signal.signal cannot be used in threads
-        signal.signal(signal.SIGTERM, termSignalHandler)
+        # necessary if using the profiler, because signal.signal cannot be
+        # used in threads
+        signal.signal(signal.SIGTERM, term_signal_handler)
     except Exception:
         pass
 
@@ -82,12 +76,30 @@ def run(options):
 
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument('-c', '--config', dest='config', default=None, metavar='b3.ini',
-                   help='B3 config file. Example: -c b3.ini')
-    p.add_argument('-u', '--update', action='store_true', dest='update', default=False,
-                   help='Update B3 database to latest version')
-    p.add_argument('-v', '--version', action='version', default=False, version=b3.getB3versionString(),
-                   help='Show B3 version and exit')
+    p.add_argument(
+        '-c',
+        '--config',
+        dest='config',
+        default=None,
+        metavar='b3.ini',
+        help='B3 config file. Example: -c b3.ini'
+    )
+    p.add_argument(
+        '-u',
+        '--update',
+        action='store_true',
+        dest='update',
+        default=False,
+        help='Update B3 database to latest version'
+    )
+    p.add_argument(
+        '-v',
+        '--version',
+        action='version',
+        default=False,
+        version=b3.getB3versionString(),
+        help='Show B3 version and exit'
+    )
 
     options, args = p.parse_known_args()
 
