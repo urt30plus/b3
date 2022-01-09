143588f1240e9cfebed2810c246ee1033a62edf5
Urt ThirtyPlus <urt30plus@gmail.com>
2022-01-09 14:31:23 -0800

    new booter plugin

---
+++ b3/plugins/booter/__init__.py
@@ -0,0 +1,30 @@
+from b3.plugins.abc import WeaponKillPlugin
+
+__author__ = '|30+|money'
+__version__ = '1.0'
+
+
+class BooterPlugin(WeaponKillPlugin):
+
+    def __init__(self, console, config=None):
+        super().__init__(console, config)
+        self._weapons = (
+            console.UT_MOD_KICKED,
+            console.UT_MOD_GOOMBA,
+        )
+
+    @property
+    def cmd_msg_prefix(self) -> str:
+        return 'bt'
+
+    @property
+    def weapon_name(self) -> str:
+        return 'boot'
+
+    @property
+    def weapon_action(self) -> str:
+        return 'booted'
+
+    @property
+    def weapons_handled(self) -> tuple[int, ...]:
+        return self._weapons
